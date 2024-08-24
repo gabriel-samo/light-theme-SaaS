3a47d61bee5d683faf44343ff3a009512f716493
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-25 02:18:10 +0300

    complete: Header Section UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,24 @@
-import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
 import "./globals.css";
-import clsx from "clsx";
+import type { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
+import { DM_Sans } from "next/font/google";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  description:
+    "Template created by Gabriel Samoylov, heavily inspired by Frontend Tribe"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" className="relative">
-      <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
+      <body className={twMerge(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
     </html>
