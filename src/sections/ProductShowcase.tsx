764bc823397335b2a617217e52ddf22fd5b79758
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-25 05:44:48 +0300

    complete: Pricing Section UI

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,14 +7,14 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost productivity</div>
           </div>
-          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] title mt-5">
+          <h2 className="section-title mt-5">
             A more effective wat to track progress
           </h2>
-          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+          <p className="section-description mt-5">
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
