f06f65980a9493bb1b783667ed040649d59a6b0a
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-25 06:53:20 +0300

    complete: Call To Action Section UI

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,40 @@
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import ArrowIcon from "@/assets/arrow-right.svg";
+
+import Image from "next/image";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+          <Image
+            src={starImage}
+            alt="Star image"
+            width={360}
+            className="absolute -left-[350px] -top-[137px]"
+          />
+          <Image
+            src={springImage}
+            alt="Spring image"
+            width={360}
+            className="absolute -right-[331px] -top-[19px]"
+          />
+        </div>
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span className="">Learn More</span>
+            <ArrowIcon className="h-5 w-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
