52e50f48c06d5d75bef7aa5e90d67b07cbff9d56
Gabriel Samoylov <gsamo122@gmail.com>
2024-08-26 01:18:43 +0300

    complete: Pricing Animation

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import CheckIcon from "@/assets/check.svg";
+
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
 const pricingTiers = [
   {
@@ -84,9 +88,20 @@ export const Pricing = () => {
                 </h3>
                 {tier.popular === true && (
                   <div className="inline-flex text-sm px-4 py-1.5 rounded-xl border border-white/20">
-                    <span className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] bg-clip-text text-transparent font-medium">
+                    <motion.span
+                      animate={{
+                        backgroundPositionX: "100%"
+                      }}
+                      transition={{
+                        duration: 1,
+                        repeat: Infinity,
+                        ease: "linear",
+                        repeatType: "loop"
+                      }}
+                      className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF)] [background-size:200%] bg-clip-text text-transparent font-medium"
+                    >
                       Popular
-                    </span>
+                    </motion.span>
                   </div>
                 )}
               </div>
